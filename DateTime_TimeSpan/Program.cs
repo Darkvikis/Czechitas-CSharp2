cd3bdc107f0aec9ee11094429f39f76d3899f8a1
Darkvikis <48491740+Darkvikis@users.noreply.github.com>
2024-04-18 18:02:12 +0200

    Add project files.

---
+++ DateTime_TimeSpan/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main()
+    {
+        // Zbyvajici dny do dovolene
+        // Postupne se uzivatele zeptej na den, mesic a rok odjezdu na dovolenou. Pomoci DateTime a TimeSpan vypocitej pocet
+        // zbyvajicich dni do odjezdu na dovolenou a vypis je na konzoli.
+        // Pozn.: pro zjednoduseni staci pouzit int.Parse
+    }
+}
