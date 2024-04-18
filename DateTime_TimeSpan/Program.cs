000aa7f2f6ac179dfba77e2f54e721024f1e8325
Darkvikis <48491740+Darkvikis@users.noreply.github.com>
2024-04-18 18:05:23 +0200

    init TryCatch

--- DateTime_TimeSpan/Program.cs
+++ DateTime_TimeSpan/Program.cs
@@ -1,12 +1,15 @@
 ﻿using System;
 
-public class Program
+namespace DateTime_TimeSpan
 {
-    public static void Main()
+    public class Program
     {
-        // Zbyvajici dny do dovolene
-        // Postupne se uzivatele zeptej na den, mesic a rok odjezdu na dovolenou. Pomoci DateTime a TimeSpan vypocitej pocet
-        // zbyvajicich dni do odjezdu na dovolenou a vypis je na konzoli.
-        // Pozn.: pro zjednoduseni staci pouzit int.Parse
+        public static void Main()
+        {
+            // Zbyvajici dny do dovolene
+            // Postupne se uzivatele zeptej na den, mesic a rok odjezdu na dovolenou. Pomoci DateTime a TimeSpan vypocitej pocet
+            // zbyvajicich dni do odjezdu na dovolenou a vypis je na konzoli.
+            // Pozn.: pro zjednoduseni staci pouzit int.Parse
+        }
     }
 }
