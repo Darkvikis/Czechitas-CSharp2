940297ca3eb11b9f3be250b39a11fef2ee52b641
Darkvikis <48491740+Darkvikis@users.noreply.github.com>
2024-05-09 18:06:40 +0200

    slovniky

--- Lekce/3_Lekce/Dictionary/Program.cs
+++ Lekce/3_Lekce/Dictionary/Program.cs
@@ -19,7 +19,85 @@ namespace Dictionary
     {
         public static void Main()
         {
+            TelefonniSeznam telefonniSeznam = new TelefonniSeznam();
 
+            Console.WriteLine("Tel cislo Jana Novaka: " + telefonniSeznam.NajdiTelefon("Jan Novak"));
+            telefonniSeznam.VypisSeznam();
+            Console.WriteLine();
+
+            telefonniSeznam.PridejKontakt("Eva Nováková", 555123456);
+            telefonniSeznam.VypisSeznam();
+            Console.WriteLine();
+
+            telefonniSeznam.SmazOsobu("Marie Novakova");
+            telefonniSeznam.VypisSeznam();
+            Console.WriteLine();
+
+            telefonniSeznam.SmazSezman();
+            telefonniSeznam.VypisSeznam();
+        }
+    }
+
+    public class TelefonniSeznam
+    {
+        private Dictionary<string, int> seznam;
+
+        public TelefonniSeznam()
+        {
+            seznam = new Dictionary<string, int>();
+
+            seznam.Add("Jan Novak", 123456789);
+            seznam.Add("Marie Novakova", 123456789);
+        }
+
+        public void PridejKontakt(string jmeno, int cislo)
+        {
+            if (seznam.TryAdd(jmeno, cislo))
+            {
+                Console.WriteLine("Uspesne pridano");
+            }
+            else
+            {
+                Console.WriteLine("Uz to existuje");
+            }
+        }
+
+        public int NajdiTelefon(string jmeno)
+        {
+            if (seznam.ContainsKey(jmeno))
+            {
+                return seznam[jmeno];
+            }
+            else
+            {
+                Console.WriteLine("Cislo neexistuje");
+                return -1;
+            }
+        }
+
+        public void VypisSeznam()
+        {
+            foreach (var osoba in seznam)
+            {
+                Console.WriteLine($"Jméno: {osoba.Key}, Telefon: {osoba.Value}");
+            }
+        }
+
+        public void SmazOsobu(string jmeno)
+        {            
+            if (seznam.Remove(jmeno))
+            {
+                Console.WriteLine("Uspesne smazano");
+            }
+            else
+            {
+                Console.WriteLine("Neexistuje");
+            }
+        }
+
+        public void SmazSezman()
+        {
+            seznam.Clear();
         }
     }
 }
